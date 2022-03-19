600e7113f6296a10d885f8834a74668c906f2baa
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-03-19 13:32:51 +0100

    need cpp14

--- explainers/__init__.py
+++ explainers/__init__.py
@@ -4,6 +4,6 @@ from .ground_truth_shap import GroundTruthShap, BruteForceKernelShap
 from .lime import Lime
 from .maple import Maple
 from .explainer_superclass import Random
+from .saabas import Saabas
+# from .breakdown import BreakDown  # need cpp14
 # from .l2x import L2X
-# from .breakdown import BreakDown
-# from .random import Random # don t need it anymore # todo [after acceptance] delete .py
--- explainers/breakdown.py
+++ explainers/breakdown.py
@@ -2,38 +2,50 @@
 Based on the official implementation at https://github.com/MI2DataLab/pyBreakDown
 """
 
-import numpy as np
 from collections import deque
+
+import numpy as np
 from blist import blist
 from pyBreakDown import explanation as e
 from tqdm import tqdm
 
-class BreakDown:
-    def __init__(self, clf, data, colnames=None, **kwargs):
-        if colnames:
-            assert len(colnames) == data.shape[1] #otherwise it wouldnt make any sense
-        self.dim = data.shape[-1]
-        self.clf = clf
-        self.data = data
-        self.colnames = colnames if colnames else [str(i) for i in range(self.dim)]
 
-    def _transform_observation (self, observation):
+class BreakDown:
+    name = 'breakdown'
+    expected_values = None
+    attribution_values = None
+    feature_importance = None
+
+    def __init__(self, trained_model, df_reference, input_names=None, **kwargs):
+        if input_names:
+            assert len(input_names) == df_reference.shape[
+                1]  # otherwise it wouldnt make any sense # todo [after acceptance] move such a test to the Test superclass
+        self.dim = df_reference.shape[-1]
+        self.trained_model = trained_model
+        self.data = df_reference
+        if isinstance(self.data, pd.DataFrame):
+            self.data = self.data.to_numpy()
+        self.colnames = input_names if input_names else [str(i) for i in range(self.dim)]
+
+    def _transform_observation(self, observation):
         if observation.ndim < 2:
             observation = np.expand_dims(observation, axis=0)
         return observation
 
     def _get_initial_dataset(self, observation, data):
         assert observation.ndim == 2 and observation.shape[0] == 1
-        return np.repeat(observation,repeats=data.shape[0], axis=0)
-    
-    def explain(self, X):
-        shap_values = np.zeros((X.shape[0], self.dim))
-        for idx, x in tqdm(enumerate(X.values)):
-            # import pdb; pdb.set_trace()
-            shap_values[idx], _ = self.explain_x(x, direction="down", useIntercept = False, baseline=0)
-        return shap_values
+        return np.repeat(observation, repeats=data.shape[0], axis=0)
 
-    def explain_x(self, observation, direction, useIntercept = False, baseline=0):
+    def explain(self, df_to_explain, **kwargs):
+        self.expected_values = np.zeros(df_to_explain.shape[0])
+
+        self.attribution_values = np.zeros((df_to_explain.shape[0], self.dim))
+        for idx, x in tqdm(enumerate(df_to_explain.values)):
+            self.attribution_values[idx], _ = self.explain_x(x, direction="down", useIntercept=False, baseline=0)
+
+        self.feature_importance = get_feature_importance(self.attribution_values)
+
+    def explain_x(self, observation, direction, useIntercept=False, baseline=0):
         """
         Make explanation for given observation and dataset.
         Method works with any sklearn prediction model
@@ -62,7 +74,7 @@ class BreakDown:
         if direction=="down":
             contributions, exp = self._explain_down(observation, baseline, data)
 
-        mean_prediction = np.mean(self.clf.predict(data))
+        mean_prediction = np.mean(self.trained_model.predict(data))
 
         if useIntercept:
             baseline = mean_prediction
@@ -75,24 +87,23 @@ class BreakDown:
         exp.make_final_prediction()
         return contributions, exp
 
-
-    def _explain_up (self, observation, baseline, data):
+    def _explain_up(self, observation, baseline, data):
         new_data = self._get_initial_dataset(observation, data)
 
-        baseline_yhat = np.mean(self.clf.predict(data))
+        baseline_yhat = np.mean(self.trained_model.predict(data))
 
-        open_variables = blist(range(0,data.shape[1]))
+        open_variables = blist(range(0, data.shape[1]))
         important_variables = deque()
         important_yhats = {}
 
-        for i in range(0, data.shape[1]):       
+        for i in range(0, data.shape[1]):
             yhats = {}
             yhats_diff = np.repeat(-float('inf'), data.shape[1])
-            
+
             for variable in open_variables:
                 tmp_data = np.copy(data)
-                tmp_data[:,variable] = new_data[:,variable]
-                yhats[variable] = self.clf.predict(tmp_data)
+                tmp_data[:, variable] = new_data[:, variable]
+                yhats[variable] = self.trained_model.predict(tmp_data)
                 yhats_diff[variable] = abs(baseline_yhat - np.mean(yhats[variable]))
 
             amax = np.argmax(yhats_diff)
@@ -108,23 +119,23 @@ class BreakDown:
         contributions = np.diff(means)
         return contributions, e.Explanation(var_names, var_values, contributions, e.ExplainerDirection.Up)
 
-    def _explain_down (self, observation, baseline, data):
+    def _explain_down(self, observation, baseline, data):
         new_data = self._get_initial_dataset(observation, data)
 
-        target_yhat = self.clf.predict(observation)
+        target_yhat = self.trained_model.predict(observation)
 
-        open_variables = blist(range(0,data.shape[1]))
+        open_variables = blist(range(0, data.shape[1]))
         important_variables = deque()
         important_yhats = {}
 
-        for i in range(0, data.shape[1]):       
+        for i in range(0, data.shape[1]):
             yhats = {}
             yhats_diff = np.repeat(float('inf'), data.shape[1])
             
             for variable in open_variables:
                 tmp_data = np.copy(new_data)
-                tmp_data[:,variable] = data[:,variable]
-                yhats[variable] = self.clf.predict(tmp_data)
+                tmp_data[:, variable] = data[:, variable]
+                yhats[variable] = self.trained_model.predict(tmp_data)
                 yhats_diff[variable] = abs(target_yhat - np.mean(yhats[variable]))
 
             amin = np.argmin(yhats_diff)
@@ -143,5 +154,5 @@ class BreakDown:
         
         return contributions, e.Explanation(var_names, var_values, contributions, e.ExplainerDirection.Down)
 
-    def _get_means_from_yhats (self, important_yhats):
-        return deque([np.array(v).mean() for k,v in important_yhats.items()])
+    def _get_means_from_yhats(self, important_yhats):
+        return deque([np.array(v).mean() for k, v in important_yhats.items()])
