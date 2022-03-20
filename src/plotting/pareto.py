df6c1df699a6bebc3a8358fd20618b6d0540c596
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-03-20 19:45:04 +0100

    add pareto line

--- src/plotting/pareto.py
+++ src/plotting/pareto.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from paretoset import paretoset
 
 from src.io import load_results
 
@@ -13,8 +14,21 @@ if __name__ == "__main__":
     eligible_points_df = get_eligible_points_df(result_df)
     summary_df = get_summary_df(result_df, score_df, eligible_points_df)
     print(summary_df.round(2))
-    ax1 = summary_df.plot.scatter(x='time',
-                                  y='percentage',
-                                  c='eligible_points')
+    min_x_axis_value = .01
+    summary_df.loc[summary_df.time < min_x_axis_value, 'time'] = min_x_axis_value
+    summary_df['explainer_name'] = summary_df.index
+    ax = summary_df.plot.scatter(x='time',
+                                 y='percentage',
+                                 # s='explainer_name', # https://www.geeksforgeeks.org/how-to-annotate-matplotlib-scatter-plots/
+                                 c='eligible_points',
+                                 )
     # must be an interactive plot
+    mask = paretoset(summary_df[['percentage', 'time']], sense=["max", "min"])
+    pareto_df = summary_df[mask].sort_values('time')
+    pareto_df.plot.line(x='time', logx=True,
+                        y='percentage',
+                        ax=ax,
+                        )
+    plt.legend(['Pareto'], loc='lower right')
+    plt.ylim([0,1])
     plt.show()
