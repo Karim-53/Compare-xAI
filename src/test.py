a011539ec1b0c576f42df5ff008c1475a47e1bcf
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-03-25 08:56:43 +0100

    add interaction

--- src/test.py
+++ src/test.py
@@ -1,12 +1,20 @@
+from src.utils import are_class_names_unique
 from tests import *
 
 valid_tests = (
-    CoughAndFever,
-    CoughAndFever1090,
-    DistributionNonUniformStatDep,
-    DistributionUniformStatDep,
-    DistributionNonUniformStatIndep,
+    DetectInteraction0,
+    DetectInteraction1,
+    DetectInteraction2,
+    DetectInteraction3,
+    DetectInteraction4,
+    # CoughAndFever,
+    # Mnist,
+    # CoughAndFever1090,
+    # DistributionNonUniformStatDep,
+    # DistributionUniformStatDep,
+    # DistributionNonUniformStatIndep,
 )
+assert are_class_names_unique(valid_tests), 'Duplicate test names'
 valid_tests_dico = {e.name: e for e in valid_tests}
 
 # not working yet
