c00f8ec93db5325bc0b4fa5b407de2194f165de5
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-04-03 07:43:50 +0200

    add link

--- reset_explainers.py
+++ reset_explainers.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import jinja2
 import pandas as pd
 
@@ -12,23 +14,54 @@ from src.utils import root
 #   another font in the table
 #   fix \n in src
 #   remove underscore (importance_symmetric) and put human readable info
+# todo [after acceptance] page for each paper: searchi ken el paper saye 9rineh w 7atineh wala mahouch yet considered, ( enehom unit tests / xai / original results eli fih)
 
 EXPLAINERS_HTML_PATH = root + "/docs/explainers/"  # todo move to src.utils
+
+import re
+
+_urlfinderregex = re.compile(r'http([^\.\s]+\.[^\.\s]*)+[^\.\s]{2,}')  # todo fix tr at the end of the link
+
+def linkify(text, maxlinklength=256):
+    def replacewithlink(matchobj):
+        url = matchobj.group(0)
+        text = str(url)
+        if text.startswith('http://'):
+            text = text.replace('http://', '', 1)
+        elif text.startswith('https://'):
+            text = text.replace('https://', '', 1)
+
+        if text.startswith('www.'):
+            text = text.replace('www.', '', 1)
+
+        if len(text) > maxlinklength:
+            halflength = maxlinklength / 2
+            text = text[0:halflength] + '...' + text[len(text) - halflength:]
+
+        return '<a class="comurl" href="' + url + '" target="_blank" rel="nofollow">' + text + '<img class="imglink" src="./images/linkout.png"></a>'
+
+    if text != None and text != '':
+        return _urlfinderregex.sub(replacewithlink, text)
+    else:
+        return ''
 
 
 def explainer_to_html(explainer_df_with_results):
     """ clean the df and write it to an html page"""
     df = explainer_df_with_results.fillna('')
-    df = df.replace([{}], '')
+    df = df.applymap(lambda x: '\n '.join(x) if isinstance(x, Iterable) and not isinstance(x, str) else x)
     # os.chdir(file_dirname)
 
     # Generate HTML from template.
     template = jinja2.Template("""<!DOCTYPE html>
     <html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <title>Demo</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <title>""" + df.loc[0, 'value'] + """</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
         </head>
@@ -40,11 +73,18 @@ def explainer_to_html(explainer_df_with_results):
         <script defer type="text/javascript">
             let myTable = new simpleDatatables.DataTable("#myTable");
         </script>
+        <style type="text/css">
+        .imglink {
+          width: 16px;
+        }
+        </style>
     </html>"""
                                )
 
-    output_html = template.render(dataframe=df.to_html(table_id="myTable"))
-
+    table_html = df.to_html(table_id="myTable")
+    table_html = linkify(table_html)
+    output_html = template.render(dataframe=table_html)
+    output_html = output_html.replace(r'\n',' <br> ')
     # Write generated HTML to file.
     file = EXPLAINERS_HTML_PATH + df.loc[0, 'value'] + ".htm"
     print(file)
@@ -53,8 +93,6 @@ def explainer_to_html(explainer_df_with_results):
 
 
 if __name__ == "__main__":
-    from explainers import Random
-
     result_df = load_results()
     result_df_detailed = detail(result_df)
 
@@ -74,8 +112,9 @@ if __name__ == "__main__":
             explainer_df,
             result_s,
         ], axis=0, ).reset_index()[cols]
-        print(explainer_df_with_results)
+        # print(explainer_df_with_results)
         explainer_df_with_results['remarks'] = None
         # todo also add columns results from original paper , and in remarks we can write why they are not the same
 
         explainer_to_html(explainer_df_with_results)
+    print('End')
