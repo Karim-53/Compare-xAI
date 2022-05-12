05183d7d4906de83028bbb2fc1b77b96b5451f79
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-05-12 09:35:31 +0200

    aggregate explainer_properties from raw and no index

--- src/aggregate_data.py
+++ src/aggregate_data.py
@@ -7,6 +7,9 @@ import numpy as np
 
 
 from src.explainer import valid_explainers_dico
+from src.export_sql import export_to_sql
+
+
 def get_required_input_data(explainer_name) -> set:
     if explainer_name not in valid_explainers_dico.keys():
         print(f'{explainer_name}  not in valid_explainers_dico.keys()')
@@ -16,6 +19,27 @@ def get_required_input_data(explainer_name) -> set:
     _explain_specific_args = explainer_class.get_specific_args_explain()
     return _init_specific_args.union(_explain_specific_args)
 
+
+def aggregate_outputs(explainer:pd.DataFrame):
+    _df = pd.DataFrame()
+    output_labels = {'importance':'Feature importance (global explanation)',
+                     'attribution':'Feature attribution (local explanation)', # can use html here
+                     'interaction':'Feature interaction (local explanation)',
+                     }
+    for out,label in output_labels.items():
+        _df[out] = explainer[f'output_{out}'].map({True:label, False:None})
+    return _df.apply(lambda x: ','.join(x.dropna().values.tolist()), axis=1)
+
+
+def aggregate_supported_models(explainer:pd.DataFrame):
+    _df = pd.DataFrame()
+    model_labels = {'model_agnostic':'Any AI model (model agnostic xAI algorithms are independent of the AI implementation)',
+                     'tree_based':'Tree-based ML',
+                     'neural_network':'Neural networks',
+                     }
+    for key,label in model_labels.items():
+        _df[key] = explainer[f'supported_model_{key}'].map({True:label, False:None})
+    return _df.apply(lambda x: ','.join(x.dropna().values.tolist()), axis=1)
 
 if __name__ == "__main__":
     result_df = load_results()
@@ -40,17 +64,20 @@ if __name__ == "__main__":
     # print(cross_tab)
     print('writing files to /data/03_experiment_output_aggregated/ ...')
     cross_tab.to_parquet('../data/03_experiment_output_aggregated/cross_tab.parquet')
-    cross_tab.to_csv('../data/03_experiment_output_aggregated/cross_tab.csv')
+    cross_tab.to_csv('../data/03_experiment_output_aggregated/cross_tab.csv', index=False)
 
     ############################################################################################################
     explainer = pd.read_csv('../data/01_raw/explainer.csv')
-    for out in ['attribution', 'importance', 'interaction']:
-        explainer[f'output_{out}'] = [None if pd.isna(s) else float(out in eval(s)) for s in explainer.outputs]
+    # todo delete supported_models column from 01_raw/explainer.csv because we add it now
+    # todo delete outputs column from 01_raw/explainer.csv because we add it now
+
+    explainer['outputs'] = aggregate_outputs(explainer)
 
     for x in ['model_agnostic', 'tree_based', 'neural_network']:
-        explainer[f'supported_model_{x}'] = [None if pd.isna(s) else x in eval(s) for s in explainer.supported_models]
         explainer[f'supported_model_{x}'] = np.logical_or(explainer[f'supported_model_{x}'],
                                                           explainer.supported_model_model_agnostic)
+
+    explainer['supported_models'] = aggregate_supported_models(explainer)
 
     explainer['required_input_data'] = [get_required_input_data(explainer_name) for explainer_name in
                                         explainer.explainer]
@@ -59,8 +86,9 @@ if __name__ == "__main__":
     for required_input_data in all_required_input_data:
         explainer[f'required_input_{required_input_data}'] = [None if pd.isna(s) else required_input_data in s for s in
                                                               explainer.required_input_data]
-    explainer.required_input_X_reference = np.logical_or(explainer.required_input_X,
-                                                         explainer.required_input_X_reference)  # todo fix that in prior code
+    if 'X' in all_required_input_data and 'X_reference' in all_required_input_data:
+        explainer.required_input_X_reference = np.logical_or(explainer.required_input_X,
+                                                             explainer.required_input_X_reference)  # todo fix that in prior code
 
     explainer.required_input_data = explainer.required_input_data.apply(str)
     # print('todo required_input_train_function is set to 0')
@@ -68,5 +96,6 @@ if __name__ == "__main__":
     print(explainer)
     print('writing explainer to /data/03_experiment_output_aggregated/ ...')
     explainer.to_parquet('../data/03_experiment_output_aggregated/explainer.parquet')
-    explainer.to_csv('../data/03_experiment_output_aggregated/explainer.csv')
+    explainer.to_csv('../data/03_experiment_output_aggregated/explainer.csv', index=False)
+    export_to_sql()
     print('End')
--- src/export_sql.py
+++ src/export_sql.py
@@ -2,32 +2,36 @@ import numpy as np
 import pandas as pd
 import sqlite3 as db
 
+def export_to_sql():
+    connection = db.connect('../data/04_sql/database')
 
-connection = db.connect('../data/04_sql/database')
+    cross_tab = pd.read_parquet('../data/03_experiment_output_aggregated/cross_tab.parquet')
+    cross_tab.loc[cross_tab.score.isna(), 'time'] = np.nan  # to not bias the average time per test
+    cross_tab.to_sql('cross_tab', connection, if_exists='replace', index=False)
 
-cross_tab = pd.read_parquet('../data/03_experiment_output_aggregated/cross_tab.parquet')
-cross_tab.loc[cross_tab.score.isna(), 'time'] = np.nan  # to not bias the average time per test
-cross_tab.to_sql('cross_tab', connection, if_exists='replace', index=False)
+    test = pd.read_csv('../data/01_raw/test.csv')
+    test.to_sql('test', connection, if_exists='replace', index=False)
 
-test = pd.read_csv('../data/01_raw/test.csv')
-test.to_sql('test', connection, if_exists='replace', index=False)
+    explainer = pd.read_parquet('../data/03_experiment_output_aggregated/explainer.parquet')
+    # explainer.replace({True:1, False:0}, inplace=True)
+    explainer = explainer.applymap(lambda x: int(x) if isinstance(x,bool) else x, na_action='ignore')
+    explainer.to_sql('explainer', connection, if_exists='replace', index=False)
 
-explainer = pd.read_parquet('../data/03_experiment_output_aggregated/explainer.parquet')
-# explainer.replace({True:1, False:0}, inplace=True)
-explainer = explainer.applymap(lambda x: int(x) if isinstance(x,bool) else x, na_action='ignore')
-explainer.to_sql('explainer', connection, if_exists='replace', index=False)
+    paper = pd.read_csv('../data/01_raw/paper.csv')
+    paper.to_sql('paper', connection, if_exists='replace', index=False)
 
-paper = pd.read_csv('../data/01_raw/paper.csv')
-paper.to_sql('paper', connection, if_exists='replace', index=False)
+    connection.close()
+    print('connection.close()')
 
-connection.close()
-print('connection.close()')
+    import shutil
+    shutil.copyfile('../data/04_sql/database', '../../cxai/src/database')
 
-import shutil
-shutil.copyfile('../data/04_sql/database', '../../cxai/src/database')
+    # verification
+    tests = cross_tab.test.unique()
+    valid_tests = test.set_index('test').loc[tests]  # if failed: some tests are not indexed in text.csv -> add them
+    assert sum(valid_tests['category'].isna()) == 0, 'there is some NaN values -> fill them'
+    # todo test that all paper tags in explainer exist in paper
 
-# verification
-tests = cross_tab.test.unique()
-valid_tests = test.set_index('test').loc[tests]  # if failed: some tests are not indexed in text.csv -> add them
-assert sum(valid_tests['category'].isna()) == 0, 'there is some NaN values -> fill them'
-# todo test that all paper tags in explainer exist in paper
+
+if __name__ == '__main__':
+    export_to_sql()
