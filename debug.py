dac7084ae8b5e8f7fae174d99a2eea5b7598615a
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-05-17 18:30:34 +0200

    Update debug.py

--- debug.py
+++ debug.py
@@ -4,9 +4,3 @@ from explainers import TreeShap
 from tests import CoughAndFever1090
 
 test = CoughAndFever1090()
-
-_explainer = TreeShap(**test.__dict__)
-_explainer.explain(test.dataset_to_explain)
-# results = test.score(**_explainer.__dict__)
-
-test.dataset_to_explain['shap_pred'] = np.array(_explainer.expected_values) + _explainer.attribution.sum(axis=1)
