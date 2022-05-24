e050de3b83ce4a24c6fe3ef6e97428cd8748be2e
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-05-24 16:13:08 +0200

    StressNbFeatures

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,3 +8,4 @@ from .pair_interaction import DetectInteraction0, DetectInteraction1, DetectInte
 from .x0_plus_x1 import DistributionNonUniformStatDep, DistributionUniformStatDep, DistributionNonUniformStatIndep
 from .dummy_axiom import CounterexampleDummyAxiom
 from .correlated_features import CorrelatedFeatures
+from .stress_nb_features import StressNbFeatures
