72ed14356b95e8fad3f9a806d98b028aaaba77ca
Kim <karim.belaid@idiada.com>
2022-08-23 16:55:11 +0200

    fix compas

--- tests/fooling_perturbation_alg_lib/__init__.py
+++ tests/fooling_perturbation_alg_lib/__init__.py
@@ -2,6 +2,10 @@ from .adversarial_models import *
 from .get_data import get_and_preprocess_compas_data
 from .utils import *
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler # is used by the calling script
+import pandas as pd
+from copy import deepcopy
 # import lime
 # import lime.lime_tabular
 # import shap
