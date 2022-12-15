7f7b71cc50034ee69f391d3821ce9a135a206095
RBorn02 <52722128+RBorn02@users.noreply.github.com>
2022-12-15 15:06:53 +0100

    Update correlated_features.py

--- tests/correlated_features.py
+++ tests/correlated_features.py
@@ -136,7 +136,66 @@ class CorrelatedFeaturesNew(Test):
             # todo add a test for attribution
         }
 
+class CorrelatedFeaturesInteraction(Test):
+    # I was not able to make this work see slide 16 / 28
+    # https://crossminds.ai/video/problems-with-shapley-value-based-explanations-as-feature-importance-measures-606f4961072e523d7b7811fc/
+    name = 'correlated_features'
+    ml_task = 'regression'
+    description = """Model: output = b^2. Feature a should have a null attribution despite the fact that it is correlated to b. """
+    input_features = ['a', 'b', 'c']
+    dataset_size = 20000
 
+    def __init__(self, **kwargs):
+        dataset_to_explain = [[np.random.randint(1, 10), i, i*2] for i in range(6)]
+        self.dataset_to_explain = pd.DataFrame(dataset_to_explain, columns=self.input_features)
+
+        def predict_func(input_features):
+            """ input should be a 2d array [data points, args] """
+            if isinstance(input_features,pd.DataFrame):
+                return (input_features['b'] + input_features['c'] + input_features['a']
+                        + interaction(input_features['a'], input_features['b'], input_features['c']))
+            if isinstance(input_features,np.ndarray):
+                return (input_features[:, 1] + input_features[:,2] + input_features[:, 0]
+                        + interaction(input_features[:,0], input_features[:,1], input_features[:,2]))
+            if isinstance(input_features,list):
+                if isinstance(input_features[0], list):
+                    return [row[1] + row[2] + row[0] + interaction(row[0], row[1], row[2]) for row in input_features]
+                else:
+                    return (input_features[1] + input_features[2] + input_features[0] 
+                            + interaction(input_features[0], input_features[1], input_features[2]))
+            else:
+                print('predict_func', input_features, type(input_features))
+                return (input_features[1] + input_features[2] + input_features[0]
+                        + interaction(input_features[0], input_features[1], input_features[2]))
+        
+        def interaction(feat1, feat2, feat3):
+            first = feat1 * feat2
+            second = feat1 * feat3
+            return first + second
+
+        truth_to_explain = predict_func(dataset_to_explain)
+        self.truth_to_explain = pd.DataFrame(truth_to_explain, columns=['target'])
+        n = self.dataset_size // len(self.truth_to_explain)
+        X = dataset_to_explain * n  # should I change this? no, because a and b should be perfectly correlated.
+
+        self.df_train = pd.DataFrame(X, columns=self.input_features)
+        self.df_reference = self.df_train  # todo change it to simple numpy if needed
+        self.df_train['target'] = truth_to_explain * n
+
+        self.trained_model = XGBRegressor(objective='reg:squarederror', n_estimators=1, max_depth=3, random_state=0,
+                                          base_score=0, eta=1)
+        self.X = self.df_train[self.input_features]
+        self.trained_model.fit(self.X, y=self.df_train.target)
+        self.X_reference = self.X
+        self.predict_func = predict_func
+        self.predict_proba = None
+
+    @classmethod
+    def score(cls, importance=None, **kwargs):
+        return {
+            'importance_is_c_dummy': importance_is_c_dummy(importance=importance),
+            # todo add a test for attribution
+        }
 
 if __name__ == '__main__':
     import sys
@@ -146,7 +205,7 @@ if __name__ == '__main__':
     from explainers.joint_shapley import JointShapley
     from explainers.saabas import Saabas
 
-    test = CorrelatedFeatures()
+    test = CorrelatedFeaturesInteraction()
     print(test.truth_to_explain, test.dataset_to_explain)
     #test.df_train['prediction'] = test.trained_model.predict(test.X)
     explainer = JointShapley(test.trained_model, test.predict_func)
