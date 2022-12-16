d2a6e218c0229a74c0df400637d15fc328626258
RBorn02 <52722128+RBorn02@users.noreply.github.com>
2022-12-16 09:25:05 +0100

    Update correlated_features.py

--- tests/correlated_features.py
+++ tests/correlated_features.py
@@ -57,8 +57,11 @@ class CorrelatedFeatures(Test):
         self.df_reference = self.df_train  # todo change it to simple numpy if needed
         self.df_train['target'] = truth_to_explain * n
 
-        self.trained_model = predict_func
+        self.trained_model = XGBRegressor(objective='reg:squarederror', n_estimators=1, max_depth=3, random_state=0,
+                                          base_score=0, eta=1)
+
         self.X = self.df_train[self.input_features]
+        self.trained_model.fit(self.X, y=self.df_train.target)
         self.X_reference = self.X
         self.predict_func = predict_func
         self.predict_proba = None
@@ -78,11 +81,11 @@ def importance_is_c_dummy(importance: list) -> float:
     a = abs(importance[0])
     b = abs(importance[1])
     c = abs(importance[2])
-    if c == 0. and b > 0. and a > 0. :
-        return 1.
-    if b == 0. or b < c:
-        return 0.
-    return round(1. - abs(c/(a+b)), 5)
+    #if c == 0. and b > 0. and a > 0. :
+    #    return 1.
+    #if b == 0. or b < c:
+    #    return 0.
+    return round(1. - (min(b, c) / max(b, c)), 5)
 
 class CorrelatedFeaturesNew(Test):
     # I was not able to make this work see slide 16 / 28
@@ -94,7 +97,7 @@ class CorrelatedFeaturesNew(Test):
     dataset_size = 20000
 
     def __init__(self, **kwargs):
-        dataset_to_explain = [[np.random.randint(1, 10), i, i*2] for i in range(6)]
+        dataset_to_explain = [[np.random.randint(1, 10), i, i] for i in range(6)]
         self.dataset_to_explain = pd.DataFrame(dataset_to_explain, columns=self.input_features)
 
         def predict_func(input_features):
@@ -146,7 +149,7 @@ class CorrelatedFeaturesInteraction(Test):
     dataset_size = 20000
 
     def __init__(self, **kwargs):
-        dataset_to_explain = [[np.random.randint(1, 10), i, i*2] for i in range(6)]
+        dataset_to_explain = [[np.random.randint(1, 10), i, i] for i in range(6)]
         self.dataset_to_explain = pd.DataFrame(dataset_to_explain, columns=self.input_features)
 
         def predict_func(input_features):
@@ -204,11 +207,16 @@ if __name__ == '__main__':
     from explainers.shap_explainer import Shap, TreeShap, ExactShapleyValues
     from explainers.joint_shapley import JointShapley
     from explainers.saabas import Saabas
+    import random
+    
+    np.random.seed(42)
+    random.seed(42)
 
-    test = CorrelatedFeaturesInteraction()
+    test = CorrelatedFeaturesNew()
     print(test.truth_to_explain, test.dataset_to_explain)
     #test.df_train['prediction'] = test.trained_model.predict(test.X)
-    explainer = JointShapley(test.trained_model, test.predict_func)
+    explainer = JointShapley(test.trained_model, test.trained_model.predict)
+    #explainer = ExactShapleyValues(test.predict_func, test.X)
     explainer.explain(test.dataset_to_explain)
     print(test.score(explainer.importance))
     print(test.df_train.drop_duplicates())
