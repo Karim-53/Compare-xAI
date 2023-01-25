7153c2ee3fa294b713370cc0858df8268cb701c2
SimonHampp <122290269+SimonH12@users.noreply.github.com>
2023-01-25 15:40:24 +0100

    added proxy task to nn_counterfactuals.py instead of in separate folder

--- tests/nn_counterfactuals.py
+++ tests/nn_counterfactuals.py
@@ -1,11 +1,159 @@
 import pandas as pd
-from xgboost import XGBRegressor
 from sklearn.datasets import make_circles
-
-from tests.counterfactuals_lib.proxy_cxAI import get_f1_score
+from sklearn.metrics import accuracy_score, f1_score
 from tests.test_superclass import Test
 from sklearn.svm import SVC
-from tests.util import importance_symmetric, is_attribution_symmetric
+from scipy import stats as st
+
+
+def get_closest_indices(dataframe, instance, mad, k=1):
+    """ Takes dataframe and instance, returns list of indices of rows closest to instance in dataframe. """
+    numeric_cols = [
+        column
+        for column in dataframe.columns
+        if dataframe[column].dtype != 'object' and dataframe[column].dtype != 'category'
+    ]
+    string_cols = [
+        column
+        for column in dataframe.columns
+        if dataframe[column].dtype == 'object' or dataframe[column].dtype == 'category'
+    ]
+    all_columns = []
+    [all_columns.append("cat") if dataframe[column].dtype == 'object' or dataframe[
+        column].dtype == 'category' else all_columns.append("cont") for column in dataframe.columns]
+
+    dataframe[f'categorical_distance'] = 0.
+
+    if string_cols:
+        for column in string_cols:
+            dataframe[f'{column}_distance'] = (dataframe[f'{column}'] != instance[column]).astype('float')
+            dataframe[f'categorical_distance'] = dataframe[f'categorical_distance'] + dataframe[f'{column}_distance']
+        dataframe[f'categorical_distance'] = dataframe[f'categorical_distance'] / len(string_cols)
+
+    dataframe[f'continous_distance'] = 0.
+    if numeric_cols:
+        index = 0
+        for column in numeric_cols:
+            mad[index] = mad[index] if mad[index] != 0 else 1
+            dataframe[f'{column}_distance'] = (abs(dataframe[column] - instance[column])) / mad[index]
+            dataframe[f'continous_distance'] = dataframe[f'continous_distance'] + dataframe[f'{column}_distance']
+            index = index + 1
+        dataframe[f'continous_distance'] = dataframe[f'continous_distance'] / len(numeric_cols)
+
+    dataframe[f'total_distance'] = dataframe[f'categorical_distance'] + dataframe[f'continous_distance']
+
+    index_smallest_distance = dataframe.nsmallest(k, f'total_distance').index.tolist()
+    all_columns = [f'{column}_distance'
+                   for column in string_cols + numeric_cols + ['categorical', 'continous', 'total']
+                   ]
+    dataframe.drop(all_columns, inplace=True, axis='columns')
+    return index_smallest_distance
+
+
+def knn_classify(dataframe, samples, to_predict, df_mad):
+    """
+    dataframe   = training data for knn (cfs+og)
+    instance    = instance to be classified
+    to_predict  = column that should be predicted
+    """
+
+    """ Select class for instance based on knn (to_predict is name of column to be predicted) """
+    prediction = []
+    numeric_cols = [
+        column
+        for column in samples.columns
+        if samples[column].dtype != 'object' and samples[column].dtype != 'category'
+    ]
+    mad = get_mad_values(df_mad[numeric_cols])
+    for i in range(len(samples.index)):
+        instance = samples.iloc[i]
+        nearest_neigbors = get_closest_indices(dataframe.drop([to_predict], axis='columns'), instance, mad)
+        prediction.append(dataframe.loc[nearest_neigbors[0]][to_predict])  # NOTE: works only for 1-NN
+    return prediction
+
+
+def classify_samples(dataframe_og, samples_og, to_predict, complete_train):
+    """ Classify samples using data in training data and return newly classified samples and accuracy compared to true labels. """
+    complete_train_copy = complete_train.copy()
+    samples = samples_og.copy()
+    dataframe = dataframe_og.copy()
+    y_test = samples[to_predict]
+    samples[to_predict] = knn_classify(dataframe, samples.drop([to_predict], axis='columns'), to_predict,
+                                       complete_train_copy)
+    samples[to_predict] = samples[to_predict].astype('int32')
+    return samples, accuracy_score(y_test, samples[to_predict]), f1_score(y_test, samples[to_predict])
+
+
+def get_mad_values(df):
+    """ Takes a dataframe as input and returns median absolute deviation of columns as list. """
+    mad_values = [
+        st.median_abs_deviation(df[column].tolist())
+        for column in df.columns
+    ]
+    return mad_values
+
+
+def get_k_closest(dataframe, instance, target_class_name, target_class, k=100):
+    """ Get k closest rows to instance from each class. """
+    numeric_cols = [
+        column
+        for column in dataframe.columns
+        if dataframe[column].dtype != 'object' and dataframe[column].dtype != 'category'
+    ]
+    string_cols = [
+        column
+        for column in dataframe.columns
+        if dataframe[column].dtype == 'object' or dataframe[column].dtype == 'category'
+    ]
+    if target_class_name in numeric_cols: numeric_cols.remove(target_class_name)
+    if target_class_name in string_cols: string_cols.remove(target_class_name)
+    mad = get_mad_values(dataframe[numeric_cols])
+    all_columns = []
+    [all_columns.append("cat") if dataframe[column].dtype == 'object' or dataframe[
+        column].dtype == 'category' else all_columns.append("cont") for column in dataframe.columns]
+
+    dataframe[f'categorical_distance'] = 0.
+    if string_cols:
+        for column in string_cols:
+            dataframe[f'{column}_distance'] = (dataframe[f'{column}'] != instance[column]).astype('float')
+            dataframe[f'categorical_distance'] = dataframe[f'categorical_distance'] + dataframe[f'{column}_distance']
+        dataframe[f'categorical_distance'] = dataframe[f'categorical_distance'] / len(string_cols)
+
+    dataframe[f'continous_distance'] = 0.
+    if numeric_cols:
+        index = 0
+        for column in numeric_cols:
+            mad[index] = mad[index] if mad[index] != 0 else 1
+            dataframe[f'{column}_distance'] = (abs(dataframe[column] - instance[column])) / mad[index]
+            dataframe[f'continous_distance'] = dataframe[f'continous_distance'] + dataframe[f'{column}_distance']
+            index = index + 1
+        dataframe[f'continous_distance'] = dataframe[f'continous_distance'] / len(numeric_cols)
+
+    dataframe[f'total_distance'] = dataframe[f'categorical_distance'] + dataframe[f'continous_distance']
+
+    df_target = dataframe.copy()
+    df_target = df_target.loc[df_target[target_class_name] == target_class]
+    df_target = df_target.nsmallest(k, f'total_distance')
+    df_original = dataframe.copy()
+    df_original = df_original.loc[df_original[target_class_name] != target_class]
+    df_original = df_original.nsmallest(k, f'total_distance')
+    df = pd.concat([df_original, df_target], ignore_index=True)
+
+    all_columns = [f'{column}_distance'
+                   for column in string_cols + numeric_cols + ['categorical', 'continous', 'total']
+                   ]
+    dataframe.drop(all_columns, inplace=True, axis='columns')
+    df.drop(all_columns, inplace=True, axis='columns')
+    return df
+
+
+def get_f1_score(counterfactuals, dataset, instance, target_class_name='label', target_class=1, k=100):
+    counterfactuals_df = pd.DataFrame(counterfactuals, columns=['x', 'y', target_class_name])
+    dataset_copy = dataset.copy()
+    dataset_closest = get_k_closest(dataset_copy, instance.iloc[0], target_class_name, target_class, k).reset_index().drop(['index'], axis='columns')
+    knn_my_metrics = pd.concat([counterfactuals_df.head(3), instance]).reset_index().drop(['index'], axis='columns')
+    _, _, f1_knn = classify_samples(knn_my_metrics, dataset_closest, target_class_name, dataset)
+    return f1_knn
 
 
 def create_data():
@@ -148,4 +296,3 @@ class CircleSmallTarget(Test):
     @classmethod
     def score(cls, instance=pd.DataFrame({'x': [0], 'y': [-1], 'label': [0]}, index=[0]), counterfactual=None, **kwargs): # TODO: how do I tell the score the instance when I only give it the cfs from the explainer?
         return {'1NN_score': get_f1_score(counterfactual, cls.dataset_to_explain, instance)}
-
