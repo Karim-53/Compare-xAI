e65524b7032d2a821a1a61d9e2368503f6427783
RBorn02 <52722128+RBorn02@users.noreply.github.com>
2023-03-23 10:42:56 +0100

    Create check_csv.py

---
+++ check_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from src.utils.import root
+
+def test(root):
+  try:
+    for path in Path(constants.root)rglob('*.csv'):
+      df = pd.read_csv(path)
+      print(df.Index.is_unique)
+      except Exception as e:
+        print('\nCSV file causing the error', path)
+        raise
+      
