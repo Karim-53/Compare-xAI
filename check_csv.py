a1445e0fa694b41c0bf28682cda264e48ccc2e5b
Kim <karim.belaid@idiada.com>
2023-03-24 08:16:56 +0100

    fix

--- check_csv.py
+++ check_csv.py
@@ -1,13 +1,16 @@
+from pathlib import Path
+
 import pandas as pd
 from src.utils import root
 
 
-def test(root='./'):
+def test_csv(root='./'):
   try:
     for path in Path(root).rglob('*.csv'):
-      df = pd.read_csv(path)
-      assert df.Index.is_unique; path
+      df = pd.read_csv(path, index_col=0)
+      assert df.index.is_unique, path
   except Exception as e:
     print('\nCSV file causing the error', path)
     raise
-      
+
+test_csv(root)
